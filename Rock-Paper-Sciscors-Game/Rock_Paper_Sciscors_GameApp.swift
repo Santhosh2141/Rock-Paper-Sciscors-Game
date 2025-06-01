b44da28a50d838ab39bfb46f0c89b49d8bf83414
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-01 19:39:39 +0530

    Initial Commit

---
+++ Rock-Paper-Sciscors-Game/Rock_Paper_Sciscors_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_Sciscors_GameApp.swift
+//  Rock-Paper-Sciscors-Game
+//
+//  Created by Santhosh Srinivas on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_Sciscors_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
